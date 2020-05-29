3821850d0084876fdfba793e1d3f678d8355e137
willowmoon <willowmoon@pop-os.localdomain>
2020-05-29 12:00:04 -0400

    getting page mdx layout started

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
 import React from "react"
+import Layout from "../components/layout"
+import { Link } from "gatsby"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <Layout>
+      Hello world!
+      <div>
+        <Link to="/post/">Post Page</Link>
+      </div>
+    </Layout>
+  )
 }
